827405604e1da19585d7b45476ee7d6fe74c5939
BlackFly25 <blackfly25@gmail.com>
2025-10-19 09:54:12 +0200

    Добавлен первый контроллер возвращающий список всех книг.

--- src/main/java/ru/blackfly/bookbuddy/controllers/BookController.java
+++ src/main/java/ru/blackfly/bookbuddy/controllers/BookController.java
@@ -1,4 +1,30 @@
 package ru.blackfly.bookbuddy.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.blackfly.bookbuddy.dto.BookDto;
+import ru.blackfly.bookbuddy.models.Book;
+import ru.blackfly.bookbuddy.services.BookService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/books")
+@Tag(name = "Books", description = "Book management APIs")
 public class BookController {
+    private final BookService bookService;
+
+    @GetMapping
+    @Operation(summary = "Get all books")
+    public ResponseEntity<List<BookDto>> getAllBooks() {
+        List<BookDto> books = bookService.findAll();
+        return ResponseEntity.ok(books);
+    }
+
 }
